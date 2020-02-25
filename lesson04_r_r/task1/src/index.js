0408891237a0fa61ae8feec5ac773d79c301650b
eposha <freedoctime@gmail.com>
2020-02-25 21:09:21 +0300

    react-redux

---
+++ lesson04_r_r/task1/src/index.js
@@ -0,0 +1,16 @@
+import store from './store';
+import { increment, decrement, reset } from './counter.actions';
+import { addUser, deleteUser, updateUser } from './users.actions';
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addUser({ id: 76, name: 'Sarah' }));
+store.dispatch(addUser({ id: 77, name: 'Tom' }));
+store.dispatch(deleteUser(77));
+store.dispatch(addUser({ id: 78, name: 'Bob' }));
+store.dispatch(updateUser(76, { name: 'Sarah Conor' }));
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(reset());
+store.dispatch(decrement());
+store.dispatch(decrement());
