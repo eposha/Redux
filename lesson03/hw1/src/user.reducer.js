e162047644e44da576a21286035ea074c1358a16
eposha <freedoctime@gmail.com>
2020-02-25 19:45:31 +0300

    fixed

--- lesson03/hw1/src/user.reducer.js
+++ lesson03/hw1/src/user.reducer.js
@@ -1,24 +1,18 @@
 import { SET_USER, REMOVE_USER } from './user.actions';
 
-const initialState = {
-    users: [
-        {
-            id: 0,
-            name: 'Tom'
-        }
-    ]
-}
+const initialState = null;
+
 
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER: return {
             ...state,
-            users: state.users.concat(action.payload.user)
+            ...action.payload.user
         }
 
         case REMOVE_USER: return {
             ...state,
-            users: state.users.filter(user => user.id !== action.payload.id)
+            users: null
         }
         default: return state
     }
