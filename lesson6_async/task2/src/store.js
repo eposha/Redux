fa027ef3585af6a18374c8058a956cd02cf6c0df
eposha <freedoctime@gmail.com>
2020-03-09 20:25:34 +0300

    5

--- lesson6_async/task2/src/store.js
+++ lesson6_async/task2/src/store.js
@@ -6,11 +6,20 @@ const reducer = combineReducers({
     users: usersReducer
 });
 
+const logger = (store) => (next) => (action) => {
+    console.group(action.type);
+    console.info('dispatching', action);
+    const result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd();
+    return result;
+};
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducer,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk), applyMiddleware(logger))
 )
 
 // const logger = store => next => action => {
