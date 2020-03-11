67a569c811787e81e70afc4482d082f486156dec
eposha <freedoctime@gmail.com>
2020-03-11 15:50:43 +0300

    5

--- lesson6_async/task2/src/users/UserInfo.jsx
+++ lesson6_async/task2/src/users/UserInfo.jsx
@@ -22,7 +22,7 @@ const UsersInfo = ({ userData, isFetching }) => {
       />
       <div className="user__info">
         <span className="user__name">{userData.name}</span>
-        <span className="user__location">{userData.location}</span>
+        <span className="user__location">{`from ${userData.location}`}</span>
       </div>
     </div>
   );
